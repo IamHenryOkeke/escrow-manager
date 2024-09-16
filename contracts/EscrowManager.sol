9fc71e45699f5272ce5316557f0f30b0f7ba5371
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-09-16 11:49:13 +0100

    implemented smart contract functionality for escrow contract

--- contracts/EscrowManager.sol
+++ contracts/EscrowManager.sol
@@ -2,5 +2,90 @@
 pragma solidity ^0.8.24;
 
 contract EscrowManager {
+    address public owner;
+    uint256 public escrowId;
+    mapping(uint256 => Escrow) public escrows;
 
+    struct Escrow {
+        address buyer;
+        address seller;
+        address arbiter;
+        uint256 amount;
+        bool buyerOk;
+        bool sellerOk;
+    }
+
+    event EscrowCreated(
+        uint256 escrowId,
+        address buyer,
+        address seller,
+        address arbiter,
+        uint256 amount
+    );
+    event BuyerOk(uint256 escrowId);
+    event SellerOk(uint256 escrowId);
+    event Withdraw(uint256 escrowId, address recipient);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createEscrow(address _seller, address _arbiter) external payable {
+        require(msg.value > 0, "Amount should be greater than 0");
+        escrows[escrowId] = Escrow(
+            msg.sender,
+            _seller,
+            _arbiter,
+            msg.value,
+            false,
+            false
+        );
+        emit EscrowCreated(escrowId, msg.sender, _seller, _arbiter, msg.value);
+        escrowId++;
+    }
+
+    function updateBuyerApproval(uint256 _escrowId) external {
+        Escrow storage escrow = escrows[_escrowId];
+        require(
+            msg.sender == escrow.buyer,
+            "Only buyer can call this function"
+        );
+        escrow.buyerOk = true;
+        emit BuyerOk(_escrowId);
+    }
+
+    function updateSellerApproval(uint256 _escrowId) external {
+        Escrow storage escrow = escrows[_escrowId];
+        require(
+            msg.sender == escrow.seller,
+            "Only seller can call this function"
+        );
+        escrow.sellerOk = true;
+        emit SellerOk(_escrowId);
+    }
+
+    function withdrawToSeller(uint256 _escrowId) external {
+        Escrow storage escrow = escrows[_escrowId];
+        require(
+            escrow.buyerOk && escrow.sellerOk,
+            "Both buyer and seller should agree"
+        );
+        address payable recipient = payable(escrow.seller);
+        recipient.transfer(escrow.amount);
+        emit Withdraw(_escrowId, recipient);
+    }
+
+    function withdrawByArbiter(uint256 _escrowId) external {
+        Escrow storage escrow = escrows[_escrowId];
+        require(msg.sender == escrow.arbiter, "Only arbiter can call this function");
+        address payable recipient = payable(escrow.arbiter);
+        recipient.transfer(escrow.amount);
+        emit Withdraw(_escrowId, recipient);
+
+    }
 }
